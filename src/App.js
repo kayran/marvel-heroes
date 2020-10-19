d9c256c1363e9d9fca71a3449326043bf49f5a0d
Kayran dos Santos <kayran@stilingue.com.br>
2020-10-18 23:52:59 -0300

    feat: create header component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import api from './api/service';
+import Header from './components/header/header';
+
+
+import './App.css';
+import HeroList from './components/heroList/heroList';
+
+const FAVORITE_LIMIT = 5;
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      heroList: [],
+      searchText: '',
+      sort:'name',
+      favorites: [],
+      onlyFavorite: false,
+      fetchingHero: false,
+      canFavorite: true,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({fetchingHero:true});
+    const heroList = await api.getHeroes();
+    this.setState({heroList, fetchingHero:false});
+
+  }
+
+  getHeroBasedOnState = async () =>{
+    console.log(this.state.onlyFavorite)
+    if(this.state.onlyFavorite){
+      return await api.getFavoriteHeroes(this.state.favorites, this.state.sort);
+    }
+    return await api.getHeroes(this.state.searchText, this.state.sort);
+  }
+
+  searchHero = async (searchText) => {
+      await this.setState({fetchingHero:true, searchText, heroList:[]});
+      const heroList = await this.getHeroBasedOnState()
+      this.setState({heroList, fetchingHero:false});
+  }
+
+  changeSort = async (sort) => {
+    await this.setState({fetchingHero:true, sort, heroList:[]});
+    const heroList = await this.getHeroBasedOnState()
+    this.setState({heroList, fetchingHero:false});
+  }
+
+  changeFavorites = async () => {
+    console.log(this.state.onlyFavorite);
+    await this.setState({
+      fetchingHero:true, 
+      onlyFavorite:!this.state.onlyFavorite, 
+      heroList:[]
+    });
+    const heroList = await this.getHeroBasedOnState()
+    this.setState({heroList, fetchingHero:false});
+  }
+
+  addFavorite = async (id) => {
+    if(!this.state.canFavorite){
+      return false;
+    }
+    const favorites = Array.from(new Set([...this.state.favorites, id]))
+    const canFavorite = favorites.length < FAVORITE_LIMIT;
+    this.setState({favorites, canFavorite});
+  }
+
+  removeFavorite = async (id) => {
+    const favorites = this.state.favorites.filter(favorite => favorite !== id)
+    const canFavorite = favorites.length < FAVORITE_LIMIT;
+    this.setState({favorites, canFavorite});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header onSearch={searchText=>this.searchHero(searchText)}/>
+        
+        <HeroList 
+          heroList={this.state.heroList} 
+          sort={this.state.sort}
+          favorites={this.state.favorites}
+          fetchingHero={this.state.fetchingHero}
+          changeSort={sort => this.changeSort(sort)}
+          changeFavorites={() => this.changeFavorites()}
+          addFavorite={id => this.addFavorite(id)}
+          removeFavorite={id => this.removeFavorite(id)}
+        />
+        <footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Search from '../search/search';
+
+import './header.css';
+export default function Header() {
+
+    return (
+        <header className='header'>
+            <img className='header__image' src='/assets/marvel.svg' alt='MARVEL search heroes logo'></img>
+            <h1 className='header__subtitle'>EXPLORE O UNIVERSO</h1>
+            <p className='header__description'>Mergulhe no dominio deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve!</p>
+            <Search onSearch={searchText=>this.searchHero(searchText)}/>
+        </header>
+    );
+}
