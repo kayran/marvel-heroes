dbbb59f8d61217eb73eb5e1c53df9bf3b03166df
Kayran dos Santos <kayran@stilingue.com.br>
2020-10-18 23:54:07 -0300

    feat: create hero list header

---
+++ src/components/heroListHeader/heroListHeader.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './heroListHeader.css'
+export default function HeroListHeader({
+    heroListCount, 
+    sort, 
+    onlyFavorites, 
+    onChangeSort, 
+    onChangeFavorites
+}) {
+    const sortLabel = (sort) => sort.startsWith('-')?'Z/A':'A/Z'
+    const changeSort = () => {
+        const newSort = sort.startsWith('-')? sort.slice(1):`-${sort}`;
+        onChangeSort(newSort);
+    }
+    return (
+        <header className='hero-list__header'>
+            <span className='hero-list__header__count'>Encontrados {heroListCount} heróis</span>
+            <span className='hero-list__header__actions'>
+                <span className='hero-list__header__actions__sort' onClick={changeSort}>
+                    <svg>
+                       <use href='#hero'/>
+                    </svg>
+                    Ordenar por nome - {sortLabel(sort)}
+                </span>
+                <span 
+                className='hero-list__header__actions__favorite'
+                onClick={onChangeFavorites}
+                >
+                   <svg>
+                       <use href='#heart-fullfilled'/>
+                    </svg> 
+                    Somente Favoritos
+                </span>
+            </span>
+        </header>
+    )
+}
